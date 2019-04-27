2200aaee28db5dfb734efd889114d3360f4176dd
Karen Pinilla <pinillakaren11@gmail.com>
2019-04-26 20:11:25 -0700

    setting up client folder

---
+++ client/containers/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+      }
+
+      render(){
+          return(
+              <div id="loginPage">LoginPage</div>
+          )
+      }
+}
+
+
+export default LoginPage
--- client/index.js
+++ client/index.js
@@ -1,1 +1,12 @@
-//start react app here
+import React from 'react'
+import { render} from 'react-dom'
+import { createStore } from 'redux'
+import StampifyApp from './reducers/reducers'
+import MainContainer from './containers/MainContainer'
+
+
+const store = createStore(StampifyApp)
+
+render(<MainContainer store={store}/>, document.getElementById('root'))
+
+
