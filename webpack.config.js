6d4efd08bbde87d0c3aca6ab3ea17722b5686270
RadicalRoy <rolandquintana1991@gmail.com>
2019-04-26 17:06:30 -0700

    added webpack, package.json, and initial client and server setups

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: './bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+      rules: [
+        // BABEL
+        {
+          test: /\.jsx?/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env', '@babel/preset-react'
+              ]
+            }
+
+          }
+        },
+        {
+          test: /\.css$/,
+          exclude: /(node_modules)/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            },
+          ]
+        },
+      ],
+    },
+    devServer: {
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000/',
+      "changeOrigin": true,
+      // pathRewrite: { '^/api': '' }
+    },
+    publicPath: '/build/'
+    // contentBase: path.join(__dirname, 'build'),
+    // compress: true,
+    // port: 8080
+  }
+}
